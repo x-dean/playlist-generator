457d3834970256ab855b35173d002bfb028c1c36
dx <deanxaka@gmail.com>
2025-07-19 09:25:11 +0200

    fix(run.sh): Correct playlist generator container name

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker run --rm \
   -v /root/music/library/by_bpm:/output \
   -v /root/music/cache:/cache \
   -e ESSENTIA_THREADS=8 \
-  playlist-generator \
+  playlist-generator-playlist-generator \
   --music_dir /music \
   --output_dir /output \
   --workers 8 \
